a0f40f33e777100347ec1bd4f5c4ad1e0d8f42f8
yngnicole <nicoley5@uci.edu>
2026-02-21 19:48:12 -0800

    Update Player.cs

--- GDIM 32 Final Project/Assets/Scripts/Player.cs
+++ GDIM 32 Final Project/Assets/Scripts/Player.cs
@@ -6,6 +6,8 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float _forwardSpeed = 1f;
     [SerializeField] private float _turnSpeed = 1f;
+    [SerializeField] private Rigidbody _playerRigidbody;
+    [SerializeField] private float _moveSpeed;
     
     // Start is called before the first frame update
     void Start()
@@ -16,11 +18,23 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //movement
+        // movement
         float translation = Input.GetAxis("Vertical") * _forwardSpeed * Time.deltaTime;
         float rotation = Input.GetAxis("Horizontal") * _turnSpeed * Time.deltaTime;
 
         transform.Translate(0, 0, translation);
         transform.Rotate(0, rotation, 0);
+        
+
+        /*float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+
+        // left/right movement 
+        Vector3 movement = (transform.forward * vertical + transform.right * horizontal).normalized;
+        _playerRigidbody.MovePosition(_playerRigidbody.position + movement * _moveSpeed * Time.deltaTime);
+        bool isWalking = Mathf.Abs(vertical) > 0.1f || Mathf.Abs(horizontal) > 0.1f;
+       */ //_animator.SetBool("IsWalking", isWalking);
     }
+
+    
 }
