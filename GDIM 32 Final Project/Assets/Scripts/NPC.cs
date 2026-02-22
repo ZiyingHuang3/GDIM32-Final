e45d74794c331cde79b77b776b349883d75823de
evelina-wang <evelinaw@uci.edu>
2026-02-22 15:58:23 -0800

    Evelina

--- GDIM 32 Final Project/Assets/Scripts/NPC.cs
+++ GDIM 32 Final Project/Assets/Scripts/NPC.cs
@@ -1,6 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-//public class NPC : MonoBehaviour//
-
+[RequireComponent(typeof(Collider))]
+public class NPC : MonoBehaviour
+{
+   [SerializeField] private int damage = 1; 
+   [SerializeField] private float damageCooldown = 1f;
+    private float lastDamageTime = -999f;
+    private void Reset()
+    {
+        var col = GetComponent<Collider>();
+        col.isTrigger = true;
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+        if (Time.time - lastDamageTime < damageCooldown) return;
+        lastDamageTime = Time.time;
+        PlayerHealth health = other.GetComponent<PlayerHealth>();
+        if (health != null)
+        {
+            health.TakeDamage(damage);
+        }
+    }
+}
