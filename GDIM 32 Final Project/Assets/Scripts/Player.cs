6fc0d74698de963f8079429ea311786389bc0ccd
yngnicole <nicoley5@uci.edu>
2026-02-22 23:52:56 -0800

    wall collider

--- GDIM 32 Final Project/Assets/Scripts/Player.cs
+++ GDIM 32 Final Project/Assets/Scripts/Player.cs
@@ -4,37 +4,31 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float _forwardSpeed = 1f;
-    [SerializeField] private float _turnSpeed = 1f;
-    [SerializeField] private Rigidbody _playerRigidbody;
-    [SerializeField] private float _moveSpeed;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    [SerializeField] private Rigidbody _playerRigidbody;
+    [SerializeField] private float _turnSpeed = 1f;
+    [SerializeField] private float _moveSpeed;
+
+    private float vertical;
+    private float horizontal;
     void Update()
     {
-        // movement
-        float translation = Input.GetAxis("Vertical") * _forwardSpeed * Time.deltaTime;
-        float rotation = Input.GetAxis("Horizontal") * _turnSpeed * Time.deltaTime;
+        vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+    }
+    private void FixedUpdate()
+    {
+        // forward/back movement 
+        Vector3 movement = transform.forward * vertical * _moveSpeed * Time.fixedDeltaTime;
+        _playerRigidbody.MovePosition(_playerRigidbody.position + movement);
 
-        transform.Translate(0, 0, translation);
-        transform.Rotate(0, rotation, 0);
-        
+        // rotate
+        float turn = horizontal * _turnSpeed * Time.fixedDeltaTime;
+        Quaternion turnRotation = Quaternion.Euler(0, turn, 0);
+        _playerRigidbody.MoveRotation(_playerRigidbody.rotation * turnRotation);    
 
-        /*float vertical = Input.GetAxis("Vertical");
-        float horizontal = Input.GetAxis("Horizontal");
-
-        // left/right movement 
-        Vector3 movement = (transform.forward * vertical + transform.right * horizontal).normalized;
-        _playerRigidbody.MovePosition(_playerRigidbody.position + movement * _moveSpeed * Time.deltaTime);
-        bool isWalking = Mathf.Abs(vertical) > 0.1f || Mathf.Abs(horizontal) > 0.1f;
-       */ //_animator.SetBool("IsWalking", isWalking);
+        //cannot move until start button is pressed
     }
 
-    
+
 }
