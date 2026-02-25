5c3087cf540a6e772072225f3a5c8fdaf447a6a9
evelina-wang <evelinaw@uci.edu>
2026-02-24 18:13:47 -0800

    Evelina

--- GDIM 32 Final Project/Assets/Scripts/ItemPickup.cs
+++ GDIM 32 Final Project/Assets/Scripts/ItemPickup.cs
@@ -2,45 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-
-
 [RequireComponent(typeof(Collider))]
 public class ItemPickup : MonoBehaviour
 {
     [Header("Item Data")]
     [SerializeField] private ItemId itemId = ItemId.Mirror;
 
+    [Header("Pickup Settings")]
+    [SerializeField] private float interactDistance = 3f; 
+
     [Header("Optional Feedback")]
     [SerializeField] private AudioClip pickupSfx;
-    [SerializeField] private AudioClip specialSfx;
-    [SerializeField] private bool playSpecialOnPickup = true;
 
-    private Camera cam;
-
-    private void Awake()
-    {
-        cam = Camera.main;
-    }
     private void OnMouseDown()
     {
-
         var player = FindObjectOfType<PlayerController>();
         if (player == null) return;
 
-        float dist = Vector3.Distance(player.transform.position, transform.position);
-        if (dist > 3f) return;
-        Pickup(player);
-    }
+        float distance = Vector3.Distance(
+            player.transform.position,
+            transform.position
+        );
 
-    private void Pickup(PlayerController player)
-    {
+        if (distance > interactDistance)
+        {
+            Debug.Log("Too far to pick up");
+            return;
+        }
+
         player.Inventory.Add(itemId);
         PlayClip(pickupSfx);
-
-        if (itemId == ItemId.MusicBox && playSpecialOnPickup)
-            PlayClip(specialSfx);
-
         gameObject.SetActive(false);
     }
 
@@ -48,11 +39,5 @@ public class ItemPickup : MonoBehaviour
     {
         if (clip == null) return;
         AudioSource.PlayClipAtPoint(clip, transform.position);
-    }
-
-    private void Reset()
-    {
-        var col = GetComponent<Collider>();
-        col.isTrigger = false; 
     }
 }
