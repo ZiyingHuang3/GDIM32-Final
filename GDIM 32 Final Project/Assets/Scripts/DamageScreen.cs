3b014bf83d3ef70d516cfe0b68d2370b5f08e36f
yngnicole <nicoley5@uci.edu>
2026-03-09 19:07:06 -0700

    red screen when damage

---
+++ GDIM 32 Final Project/Assets/Scripts/DamageScreen.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DamageScreen : MonoBehaviour
+{
+    [SerializeField] private Image overlay;
+    [SerializeField] private float flashAlpha = 0.5f;
+    [SerializeField] private float fadeSpeed = 4f;
+
+    private void Awake()
+    {
+        overlay.enabled = false;
+    }
+    private void OnEnable()
+    {
+        GameEvents.OnHealthChanged += OnHealthChanged;
+        GameEvents.OnGameStarted += OnGameStarted;
+    }
+
+    private void OnDisable()
+    {
+        GameEvents.OnHealthChanged -= OnHealthChanged;
+    }
+
+    private void OnGameStarted()
+    {
+        overlay.enabled = true;
+    }
+    private void OnHealthChanged(int health)
+    {
+        Flash();
+        Debug.Log("Damage Flash Triggered");
+    }
+    private void Flash()
+    {
+        StopAllCoroutines();
+        StartCoroutine(FlashRoutine());
+    }
+
+    IEnumerator FlashRoutine()
+    {
+        Color color = overlay.color;
+        color.a = flashAlpha;
+        overlay.color = color;
+
+        while (overlay.color.a > 0)
+        {
+            color.a -= Time.deltaTime * fadeSpeed;
+            overlay.color = color;
+            yield return null;
+        }
+    }
+}
