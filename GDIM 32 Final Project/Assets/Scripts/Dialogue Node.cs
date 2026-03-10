5e0a27b6cebb60b2151c6e53e369dfdfe498ef99
yngnicole <nicoley5@uci.edu>
2026-03-09 18:42:42 -0700

    scriptableobecjt

---
+++ GDIM 32 Final Project/Assets/Scripts/Dialogue Node.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DialogueLine", menuName = "ScriptableObjects/DialogueLine", order = 1)]
+public class DialogueNode : ScriptableObject
+{
+    
+    public string[] _lines;
+    public string[] _playerReplyOptions;
+    public DialogueNode[] _npcReplies;
+}
